27d16662390df686a289d1fcf5e1aae734bed2d7
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2024-11-03 16:51:29 +0900

    テンプレートを登録した。

---
+++ init.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Redmine::Plugin.register :template_redmine_plugin do
+  name 'Template Redmine Plugin'
+  author '9506hqwy'
+  description 'This is a template plugin for Redmine'
+  version '0.1.0'
+  url 'https://github.com/9506hqwy/template_redmine_plugin'
+  author_url 'https://github.com/9506hqwy'
+end
