0e0fa47755d219fa0300a1f3267262022fc263f3
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-05-20 20:41:01 +0900

    ワークフローに playwright の実行を追加した。

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,15 +6,34 @@ export default defineConfig({
   reporter: 'html',
   projects: [
     {
+      name: 'initialize',
+      testMatch: [
+        'initialize.spec.ts'
+      ],
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
       name: 'chromium',
+      testIgnore: [
+        'initialize.spec.ts'
+      ],
+      dependencies: ['initialize'],
       use: { ...devices['Desktop Chrome'] },
     },
     {
       name: 'firefox',
+      testIgnore: [
+        'initialize.spec.ts'
+      ],
+      dependencies: ['initialize'],
       use: { ...devices['Desktop Firefox'] },
     },
     {
       name: 'webkit',
+      testIgnore: [
+        'initialize.spec.ts'
+      ],
+      dependencies: ['initialize'],
       use: { ...devices['Desktop Safari'] },
     },
   ],
